b47046b7516696bfdfc9e8f5c97885f9d3c33eb3
pnaveenkp <naveenkumar123243@gmail.com>
2023-06-08 12:00:55 +0530

    made correction in ec2 keypair

--- main.tf
+++ main.tf
@@ -115,14 +115,14 @@ resource "aws_instance" "web-1" {
     #ami = "${data.aws_ami.my_ami.id}"
     availability_zone = "us-east-1a"
     instance_type = "t2.micro"
-    key_name = "LaptopKey"
+    key_name = var.key_name
     subnet_id = "${aws_subnet.subnet1-public.id}"
     vpc_security_group_ids = ["${aws_security_group.allow_all.id}"]
     associate_public_ip_address = true	
     tags = {
         Name = "Server-1"
-        Env = "Prod"
-        Owner = "Sree"
+        Env = "Dev"
+        Owner = "Naveen"
 	CostCenter = "ABCD"
     }
 }
